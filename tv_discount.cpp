121d265ad86b958eb03dea7e8c42ce5debaa3fd9
gerry2206 <gerrymsupa78@gmail.com>
2022-11-05 21:00:15 +0530

    cc soln

--- tv_discount.cpp
+++ tv_discount.cpp
@@ -7,8 +7,8 @@ int main(){
     while(t--){
         int a, b, c, d;
     cin >> a >> b >> c >> d;
-    if((a - c) > (b - d)) cout << "First" << endl;
-    else if( (a - c) < (b - d)) cout << "Second" << endl;
+    if((a - c) < (b - d)) cout << "First" << endl;
+    else if( (a - c) > (b - d)) cout << "Second" << endl;
     else cout << "Any" << endl;
 
     }
