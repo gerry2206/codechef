6be67655a133f7675328627a28b4f067926fa661
gerry2206 <gerrymsupa78@gmail.com>
2022-11-25 17:14:55 +0530

    cc soln

---
+++ aorb.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define forn for(int i= 0; i<n; ++i)
+#define fornn for(int i=0; i<= n; ++i)
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    
+    if( a >= b ) cout << 1000 - ( b * 4 ) + 500 - ( (a + b) * 2 ) << endl;
+    else cout << 500 - ( a * 2 ) + 1000 - ( ( a + b ) * 4 ) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    for(int i = 1; i <= tc; i++) {
+        solve();
+    }
+    
+    return 0;
+}
